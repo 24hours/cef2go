c228287a3276bda6da5f35e13345ebede8847775
sindrosa <sindrosa.24@gmail.com>
2015-02-11 15:19:27 +0900

    add test case

--- chrome_test.go
+++ chrome_test.go
@@ -1,10 +1,16 @@
-package chrome
+package chrome_test
 
 import (
+	"github.com/24hours/chrome"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestingAppHandler(t testing.T) {
-	assert.Equal(1, 1, "1 == 1 should be always true")
+func TestBasic(t *testing.T) {
+	assert.Equal(t, 1, 1, "1 == 1 should be always true")
+}
+
+func TestBase(t *testing.T) {
+	assert.Equal(t, -1, chrome.ExecuteProcess(nil, nil), "ExecuteProcess should return -1")
+	chrome.Shutdown()
 }
