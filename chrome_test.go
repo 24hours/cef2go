a68d3841428fe51a25834cbdae11ffc7b519a936
sindrosa <sindrosa.24@gmail.com>
2015-02-12 09:33:21 +0900

    do not use shutdown yet

--- chrome_test.go
+++ chrome_test.go
@@ -39,5 +39,5 @@ func TestBase(t *testing.T) {
 	settings := chrome.NewSettings()
 	settings.NoSandbox = 1
 	assert.Equal(t, 1, chrome.Initialize(settings, nil), "Initialize")
-	chrome.Shutdown()
+	//chrome.Shutdown()
 }
