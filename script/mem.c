6ba88094e6654cc5423200ebd1b3904eeb23762f
Lee Yeong Khang <sindrosa.24@gmail.com>
2015-06-12 02:44:20 +0900

    ensure only minimum code working

--- script/mem.c
+++
@@ -1,5 +0,0 @@
-int main(){
-    //something something
-    int a = 4; // raw_cm
-    int b = 1;
-}
--- script/runner.sh
+++ script/runner.sh
@@ -1,2 +1,5 @@
-#python auto_bind.py ../include/capi/cef_app_capi.h -I../> dump.txt
-python generate.py main.h -I../> dump.txt
+# python auto_bind.py ../include/capi/cef_app_capi.h -I../> dump.txt
+# python cef_parser.py main.h -I../ > dump.js
+# python cef_parser.py main.h -I../ > dump.txt
+python cef_parser.py main.h -I../
+tput bel
